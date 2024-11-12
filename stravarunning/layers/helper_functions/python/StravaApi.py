a27f39c209fc1d5a2bf1526d15630830eefdce8b
Pawan Kumar <pavan11896@gmail.com>
2024-11-12 16:00:15 +0530

    Updated with Error Handling

--- stravarunning/layers/helper_functions/python/StravaApi.py
+++ stravarunning/layers/helper_functions/python/StravaApi.py
@@ -11,119 +11,97 @@ logger.setLevel("INFO")
 
 class StravaAPI:
     def __init__(self, config_path=None):
-        self.access_token = os.environ["StravaAccessToken"]
         self.refresh_token = os.environ["StravaRefreshToken"]
-        self.client = self.setUpClient()
+        self.auth_url = "https://www.strava.com/oauth/token"
+        self.athlete_activities_url = "https://www.strava.com/api/v3/athlete/activities"
+        self.activities_url = "https://www.strava.com/api/v3/activities"
+        self.refreshAccessToken()
+        self.access_token = os.environ["StravaAccessToken"]
+        self.headers = {'Authorization': f'Bearer {self.access_token}'}
         self.utils = CommonUtils()
         self.ist = pytz.timezone('Asia/Kolkata')
-        
-    def setUpClient(self):
-        client = Client()
-        client.access_token = self.access_token
-        return client        
+             
 
     def updateEnvironmentTokens(self):
         os.environ["StravaAccessToken"] = self.access_token
         os.environ["StravaRefreshToken"] = self.refresh_token
 
     def refreshAccessToken(self):
-        response = requests.post(
-            'https://www.strava.com/oauth/token',
-            data={
-                'client_id': os.environ['StravaClientId'],
-                'client_secret': os.environ['StravaClientSecret'],
-                'grant_type': 'refresh_token',
-                'refresh_token': self.refresh_token,
-            }
-        )
+        response = requests.post(self.auth_url, data={
+            'client_id': os.environ['StravaClientId'],
+            'client_secret': os.environ['StravaClientSecret'],
+            'grant_type': 'refresh_token',
+            'refresh_token': self.refresh_token
+            })
         if response.status_code == 200:
             tokens = response.json()
             self.access_token = tokens['access_token']
             self.refresh_token = tokens['refresh_token']
-            self.client.access_token = self.access_token
             self.updateEnvironmentTokens()
         else:
             logger.error(f"[Strava]: Code {response.status_code} {response.text}")
             raise NoDataError(code = response.status_code, msg = response.text)
                 
     def getLastSavedActivity(self):
-        try:
-            headers = {
-                'Authorization': f'Bearer {self.access_token}'
-            }
-            response = requests.get('https://www.strava.com/api/v3/athlete/activities', headers=headers)
-            response.raise_for_status()
-            return response.json()[0]
-        except Exception as e:
-            logger.warning(f"[Strava]: {response.status_code} {response.text}")
-            self.refreshAccessToken()
-            return self.getLastSavedActivity()
-    
-    def getActivityById(self, activity_id):
-        try:
-            headers = {
-                'Authorization': f'Bearer {self.access_token}'
-            }
-            response = requests.get(f'https://www.strava.com/api/v3/activities/{activity_id}', headers=headers)
-            response.raise_for_status()
-            return response.json()
-        except Exception as e:
-            logger.warning(f"[Strava]: {response.status_code} {response.text}")
-            self.refreshAccessToken()
-            return self.getActivityById(activity_id)
-        
-    def getYesterdayActivity(self, type = "Run"):
-        before, after = datetime.today(), datetime.today() - timedelta(days=2)
-        headers = {
-            'Authorization': f'Bearer {self.access_token}'
-        }
-
-        params = {
-            'before' : before,
-            'after' : after
-        }
-            
-        response = requests.get(f'https://www.strava.com/api/v3/activities/', headers=headers, params = params)
+        response = requests.get(self.athlete_activities_url, headers= self.headers)
         if response.status_code == 200:
             return response.json()
+        elif response.status_code == 401:
+            logger.warning(f"[Strava]: {response.status_code} {response.text}\n Refreshing Tokens")
+            self.refreshAccessToken()
+            return self.getLastSavedActivity()
+        else:
+            logger.error(f"[Strava]: Code {response.status_code} {response.text}")
+            raise NoDataError(code = response.status_code, msg = response.text)
+                        
+    def getActivityById(self, activity_id):
+        response = requests.get(f'{self.activities_url}/{activity_id}', headers= self.headers)
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 401:
+            logger.warning(f"[Strava]: {response.status_code} {response.text}\n Refreshing Tokens")
+            self.refreshAccessToken()
+            return self.getActivityById(activity_id)
+        else:
+            logger.error(f"[Strava]: Code {response.status_code} {response.text}")
+            raise NoDataError(code = response.status_code, msg = response.text)
+                   
+    def getYesterdayActivity(self, type = "Run"):
+        before, after = datetime.today(), datetime.today() - timedelta(days=2)
+        response = requests.get(f'{self.activities_url}/', headers=self.headers, params = {'before' : before,'after' : after})
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 401:
+            logger.warning(f"[Strava]: {response.status_code} {response.text}\n Refreshing Tokens")
+            self.refreshAccessToken()
+            return self.getYesterdayActivity()
         else:
             logger.error(f"[Strava]: Code {response.status_code} {response.text}")
             raise NoDataError(code = response.status_code, msg = response.text)
         
     def getActivitiesInDateRange(self, after, before):
-        before = datetime.strptime(before, "%Y-%m-%d")
-        after = datetime.strptime(after, "%Y-%m-%d")
-        epoch_before = int(before.timestamp())
-        epoch_after = int(after.timestamp())
-        try:
-            headers = {
-                'Authorization': f'Bearer {self.access_token}'
-            }
-
-            params = {
-                'before' : epoch_before,
-                'after' : epoch_after
-            }
-                
-            response = requests.get(f'https://www.strava.com/api/v3/activities/', headers=headers, params = params)
-            response.raise_for_status()
+        epoch_before = int(datetime.strptime(before, "%Y-%m-%d").timestamp())
+        epoch_after = int(datetime.strptime(after, "%Y-%m-%d").timestamp())            
+        response = requests.get(f'{self.activities_url}/', headers=self.headers, params = {'before' : epoch_before, 'after' : epoch_after})
+        if response.status_code == 200:
             return response.json()
-        except Exception as e:
-            print(f"Error fetching activity by ID: {e}")
+        elif response.status_code == 401:
+            logger.warning(f"[Strava]: {response.status_code} {response.text}\n Refreshing Tokens")
             self.refreshAccessToken()
-            return self.getActivitiesInDateRange(self, after, before)
+            return self.getActivitiesInDateRange(after, before)
+        else:
+            logger.error(f"[Strava]: Code {response.status_code} {response.text}")
+            raise NoDataError(code = response.status_code, msg = response.text)
         
     def getLastSavedRun(self):
-        try:
-            headers = {
-                'Authorization': f'Bearer {self.access_token}'
-            }
-            response = requests.get('https://www.strava.com/api/v3/athlete/activities', headers=headers)
-            response.raise_for_status()
+        response = requests.get(self.athlete_activities_url, headers=self.headers)
+        if response.status_code == 200:
             last = [i for i in response.json() if i['type'] == "Run"][0]
             return last
-        except Exception as e:
-            print(f"Error fetching all activities: {e}")
+        elif response.status_code == 401:
+            logger.warning(f"[Strava]: {response.status_code} {response.text}\n Refreshing Tokens")
             self.refreshAccessToken()
             return self.getLastSavedRun()
-
+        else:
+            logger.error(f"[Strava]: Code {response.status_code} {response.text}")
+            raise NoDataError(code = response.status_code, msg = response.text)
